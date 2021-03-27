62bea1a3b2c82740b5374b9e53e55c32bb7f3ebb
sleekEagle <lahirunuwanisme@gmail.com>
2021-03-27 17:37:48 -0400

    start synth

--- number_extract.py
+++ number_extract.py
@@ -20,13 +20,13 @@ for i in range(0,1000):
 num=99
 def get_mapped(num,min_num,max_num,num_notes):
     period=(max_num-min_num)/num_notes
-    
+
     mapped=0
     for i in range(1,num_notes+1):
-        print('___')
-        print(period*i)
         if(num<=period*i):
             mapped=i
             break
     return mapped
+
+
 
