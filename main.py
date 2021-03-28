262a71480de30c60b7175c359f310f1792f86bcb
sleekEagle <lahirunuwanisme@gmail.com>
2021-03-28 10:13:01 -0400

    adding main

--- main.py
+++ main.py
@@ -10,68 +10,89 @@ import extract_interval_numbers
 import synth  
 from pyo import *
 import time
+import os
 
-
-        
 def get_mapped_value(val,val_min=0,val_max=99,map_min=20,map_max=2000):
     m=(map_max-map_min)/(val_max-val_min)
     c=map_max-m*val_max
     mapped=m*val+c
-    return mapped       
+    return mapped  
 
-file='C:/Users/Owner/code/number_song/sqrt3.txt' 
-#these are for chord,lead,lead2,base   
-take_avg_of=[1,2,3,16]        
-pop=extract_interval_numbers.PopulateNumbers(file,take_avg_of,10000)
-pop.start_thread()
-pop.get_numbers(0)
-pop.kill_thread()
-pop.num_lists[0]
-
-s = Server().boot()
-s.start()
-
-base=synth.Synth(fadein=0,fadeout=0,dur=1.6,mul=0.5,weights=[0.9,0.5,5])
-lead2=synth.Synth(fadein=0,fadeout=0,dur=0.3,mul=0.5,weights=[0.9,0.5,5])
-lead=synth.Synth(fadein=0,fadeout=0,dur=0.2,mul=0.5,weights=[0.9,0.5,5])
-chord=synth.Synth(fadein=0,fadeout=0,dur=0.1,mul=0.5,weights=[0.9,0.5,5])
-synth_list=[chord,lead,lead2,base]
+def main():
+    #setting up file paths
+    dir = os.path.dirname(synth.__file__)
+    progression_file=os.path.join(dir, 'progression.txt')
+    sqrt_file=os.path.join(dir, 'sqrt3.txt')
 
 
-counter_list=counter_list=[0]*(len(take_avg_of))
-while True:
-    for i,item in enumerate(counter_list):
-        if(item==0):
-            num=pop.get_numbers(i)
-            synth_list[i].play(num)
-    counter_list=[item+1 for item in counter_list]
-    index_list=[i for i,item in enumerate(counter_list) if (i!=0) and (item==take_avg_of[i])]                   
-    for index in index_list:
-        counter_list[index]=0
-    time.sleep(0.1)
+    #read parameters
+    f = open(progression_file, "r")
+    lines=f.readlines()
+    progression=[]
+    for item in lines:
+        t=item.split(':')[0]
+        play=(item.split(':')[-1])[0:7].split(',')
+        progression.append([t,play])
+    
+      
+     
+
+    #these are for chord,lead,lead2,base   
+    take_avg_of=[1,2,3,16]        
+    pop=extract_interval_numbers.PopulateNumbers(sqrt_file,take_avg_of,10000)
+    time.sleep(1)
+    pop.start_thread()
+    time.sleep(1)
+    pop.get_numbers(0)
+
+
+    s = Server().boot()
+    s.start()
+    
+    base=synth.Synth(fadein=0.02,fadeout=0.02,dur=1.6,mul=0.5,weights=[0.08,0.8,0.2])
+    lead2=synth.Synth(fadein=0.3,fadeout=0,dur=0.3,mul=0.5,weights=[0.8,0.1,0.1])
+    lead=synth.Synth(fadein=0,fadeout=0.2,dur=0.2,mul=0.5,weights=[0.6,0.2,0.1])
+    chord=synth.Synth(fadein=0.01,fadeout=0.09,dur=0.1,mul=0.5,weights=[0.5,0.8,0.1])
+    synth_list=[chord,lead,lead2,base]
+      
+    count=0
+    while True:
+        t=count*0.1
+        if(t>=int(progression[-1][0])):
+            break
+        prog_index=[i for i,p in enumerate(progression) if t<int(p[0])][0]
+        is_base,is_lead,is_lead2,is_chord=progression[prog_index][1]
+        if(count%take_avg_of[0]==0):
+            num=pop.get_numbers(0)
+            num=get_mapped_value(num,val_min=0,val_max=99,map_min=240,map_max=720)
+            if(int(is_chord)):
+                chord.play(num)
+            time.sleep(0.1)
+           
+        if(count%take_avg_of[1]==0):
+            num=pop.get_numbers(1)
+            num=get_mapped_value(num,val_min=0,val_max=99,map_min=80,map_max=240)
+            if(int(is_lead)):
+                lead.play(num)
+        
+        if(count%take_avg_of[2]==0):
+            num=pop.get_numbers(2)
+            num=get_mapped_value(num,val_min=0,val_max=99,map_min=40,map_max=120)
+            if(int(is_lead2)):
+                lead2.play(num)
+          
+        if(count%take_avg_of[3]==0):
+            num=pop.get_numbers(3)
+            num=get_mapped_value(num,val_min=0,val_max=99,map_min=20,map_max=60)
+            if(int(is_base)):
+                base.play(num)
             
-    
-    index_list=[i for i,item in enumerate(counter_list) if (i!=0) and (item==take_avg_of[i])]                   
-    for index in index_list:
-        counter_list[index]=0
-    time.sleep(1.6)
-       
-
-
-get_mapped_value(99,val_min=0,val_max=99,map_min=100,map_max=1000)
-
-
-base.play(45)
-print(21)
-lead.play(300)
-
-
+        count+=1
     
 
 
-
-
-
+if __name__ == "__main__":
+    main()
 
 
 
